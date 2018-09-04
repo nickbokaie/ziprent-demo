5f9d2ccc5e768f5bcc36ed101581edb976a6272e
Nicholas Bokaie <nicholasbokaie@Nicholass-MacBook-Pro.local>
2018-09-04 13:35:36 -0700

    update to new property page

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,14 @@
 // This is where it all goes :)
+sr.reveal('.img-reveal', { duration: 1500 });
+
+$(document).ready(function(){
+    $('.property-img-wrapper').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      autoplay: true,
+    });
+  });
+
